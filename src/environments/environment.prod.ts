9813685fb689cae0498cda913e07a1dbb02661e2
Joseph Andrade <joe.seph.andrade.ii@gmail.com>
2025-03-10 17:00:33 +0800

    deploy prod build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+const baseURL = "https://staging.acps.com.au/api"
+const baseURL_AUTH = "https://staging.acps.com.au/api/auth"
+
+export const environment = {
+  production: false,
+  API_URL:  baseURL,
+  API_AUTH: baseURL_AUTH,
+  WP_URL: 'https://wp.acps.com.au',
+  SENTRY_DSN: 'https://cd381a567a8c1442c54e8e34c1c244d5@o4507586670559232.ingest.us.sentry.io/4507586677374976',
+  ACCESS_HEADER: 'X-Access-Token',
+  ACCESS_TOKEN:'csj9ibynqLWnGPm-CZGZzdbXYv2EskL4BJ2A2uy177Q=',
+  SECRET_KEY: "SPCA-TNEMPOLEVED",
+  EWAY_PUB_KEY: "/aOKJdFTZz+c2XW2sialjCaoWTwOicUMoQ7C+L5Hb58JIM7YoyuZOZsPn92HDUeKGlhJEK9uvKz/7Bg3NWS42sms/Y3Pl3d7e90SaxoyUqHKUr5nRCJ1iVcUF8s6SXFJ9OJZJYwiPIrNunAg059a5mcoxBY3BOxENjcChXoileEcsPl+6Yg0qVYx8Z1Y7Rj46EgHvRebfWsUj/5iiQZ+2tSDYQyxDN0ozpqrUcfJZWNegCDUrprilTELK5Ha1ODuSV2zFX8dku4M54EmJyGVzTCW/RFGTIXM9HNCrDCuuWiYYAjoy2T4Vscl5bkrSOOTmXdSFbtm+t6tK0g9osJXrQ==",
+  addNoIndexMeta: true, // Add this property,
+  DIRECT_LINK: 'https://staging.acpsfe.jezweb.xyz/members-only'
+};
